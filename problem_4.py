4ebb6820885f7b735210215b04ff83f577cbce6a
David Wilhelm <dave@jumbledpile.com>
2010-10-31 09:16:51 -0700

    p4: add brute force solution

---
+++ problem_4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+A palindromic number reads the same both ways. The largest palindrome
+made from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+import sys
+
+
+def ispalindrome(n):
+    digit = str(n)
+    for i in range((len(digit) + 1) / 2):
+        if digit[i] != digit[-i - 1]:
+            return False
+    return True
+
+
+def run(ndigits):
+    pals = {}
+    for x in xrange(10 ** (ndigits - 1), 10 ** ndigits):
+        for y in xrange(x, 10 ** ndigits):
+            if ispalindrome(x * y):
+                pals[x * y] = (x, y)
+    largest = max(pals.keys())
+    #print sorted(pals.keys())
+    print '%d = %d * %d' % ((largest,) + pals[largest])
+
+
+def main():
+    if len(sys.argv) > 1:
+        run(int(sys.argv[1]))
+    else:
+        run(2)
+        run(3)
+
+
+if __name__ == '__main__':
+    main()
