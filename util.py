783d848d16319b7c64d7dbdbeb5f7a41a72c2aca
David Wilhelm <dave@jumbledpile.com>
2010-11-04 14:09:54 -0700

    util: add function to return prime factorization

--- util.py
+++ util.py
@@ -59,3 +59,26 @@ def gennewprimes(n):
             primes.append(p)
             added += 1
 
+
+def getprimefactors(n):
+    """Return the prime factorization of the input n."""
+    assert n > 1
+    primefactors = {}
+    count = 0
+    while n & 1 == 0:
+        count += 1
+        n >>= 1
+    if count:
+        primefactors[2] = count
+        count = 0
+    for x in xrange(3, int(math.floor(math.sqrt(n))) + 1, 2):
+        while n % x == 0:
+            count += 1
+            n /= x
+        if count:
+            primefactors[x] = count
+            count = 0
+    if n > 1:
+        primefactors[n] = 1  # n is prime
+    return primefactors
+
