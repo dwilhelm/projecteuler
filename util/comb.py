c7ec23b4535c634efcc000eb25464d70a28d85e7
David Wilhelm <dave@jumbledpile.com>
2010-11-15 19:07:47 -0800

    comb: add permutation generator

--- util/comb.py
+++ util/comb.py
@@ -12,3 +12,16 @@ def seqoflen(n, items):
         for seq in seqoflen(n - 1, items):
             yield [item] + seq
 
+
+def permoflen(n, items):
+    """Generate all permutations of length n from items as lists."""
+    assert type(n) == int
+    if not n:
+        yield []
+        return
+    for item in list(items):
+        remaining = list(items)
+        remaining.remove(item)
+        for seq in permoflen(n - 1, remaining):
+            yield [item] + seq
+
